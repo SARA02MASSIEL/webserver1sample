a83075b651d671fe9c0dd26bdbafea90dc1a0481
Sara <saramassiel50@gmail.com>
2026-02-23 14:06:18 -0400

    hello world

--- src/app.js
+++ src/app.js
@@ -3,4 +3,9 @@ import express from "express";
 const app = express();
 const port = process.env.port || 3000;
 
+app.get("/", (req, res) => {
+    res.send("Hello world");
+})
+
 app.listen(port, () => {console.log(`Server is running on http://localhost:${port}`)})
+
