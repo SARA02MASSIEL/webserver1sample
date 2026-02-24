8726ed87db0c887829f8e1c922936baa18118940
Sara <saramassiel50@gmail.com>
2026-02-24 08:40:30 -0400

    index

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.port || 3000;
 
+app.use(express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), "public")));
+app.set("views", path.join(path.dirname(fileURLToPath(import.meta.url)), "views"));
+app.set("view engine", ejs);
+
+
 app.get("/", (req, res) => {
     res.send("Hello world");
-})
+});
 
-app.listen(port, () => {console.log(`Server is running on http://localhost:${port}`)})
+app.listen(port, () => {console.log(`Server is running on http://localhost:${port}`)});[[[[][[]]]]]
 
